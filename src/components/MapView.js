235fd374ceff0e15e499a4bf3a2ca73b639f9813
Julia Tan <juliajtan@gmail.com>
2020-09-22 17:06:09 +0100

    Render markers with DivIcon that accepts dynamic information

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,52 +1,87 @@
 import React, { Component } from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import { Icon } from 'leaflet';
+import { Map, Marker, TileLayer } from 'react-leaflet';
+import { DivIcon } from 'leaflet';
 import { fetchCountryData } from '../api/coronaApi';
 import 'leaflet/dist/leaflet.css'; // required to style leaflet map
-
-export const icon = new Icon({
-  iconUrl: '/marker.svg',
-  iconSize: [50, 50],
-});
+import './MapView.scss';
 
 export default class MapView extends Component {
   state = {
     currentLocation: { lat: 51.5, lng: -0.1 }, // set to London
     zoom: 3,
-    activeCountry: null,
+    // activeCountry: null,
     countryData: null,
   };
 
   componentDidMount() {
-    this.countryData();
+    this.loadCountryData();
   }
 
-  countryData = async () => {
-    const response = await fetchCountryData();
-    console.log(response);
-    const results = response.features;
-    this.setState({ countryData: results });
+  loadCountryData = async () => {
+    const geoJson = await fetchCountryData();
+    this.setState({ countryData: geoJson.features });
   };
 
-  setActiveCountry = (country) => {
-    this.setState({ activeCountry: country });
-  };
+  // setActiveCountry = (country) => {
+  //   this.setState({ activeCountry: country });
+  // };
 
   render() {
     let countryMarkers = null;
     if (this.state.countryData) {
-      countryMarkers = this.state.countryData.map((countryObj) => (
-        <Marker
-          // need to refactor
-          key={`lat${countryObj.properties.countryInfo.lat}long${countryObj.properties.countryInfo.long}updated${countryObj.properties.updated}`}
-          position={[
-            countryObj.properties.countryInfo.lat,
-            countryObj.properties.countryInfo.long,
-          ]}
-          onClick={() => this.setActiveCountry(countryObj)}
-          icon={icon}
-        />
-      ));
+      countryMarkers = this.state.countryData.map((countryObj) => {
+        const {
+          country,
+          updated,
+          cases,
+          deaths,
+          recovered,
+          countryInfo,
+        } = countryObj.properties;
+
+        let casesFormatted = `${cases}`;
+        let updatedFormatted;
+
+        if (cases > 1000) {
+          casesFormatted = `${casesFormatted.slice(0, -3)}k+`;
+        }
+
+        if (updated) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
+
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${casesFormatted}
+            </span>
+        `;
+
+        return (
+          <Marker
+            key={`lat${countryInfo.lat}long${countryInfo.long}updated${updated}`}
+            position={[countryInfo.lat, countryInfo.long]}
+            // onClick={() => this.setActiveCountry(countryObj)}
+            icon={
+              new DivIcon({
+                // iconUrl: '/marker.svg',
+                // iconSize: [50, 50],
+                className: 'icon',
+                html,
+              })
+            }
+            riseOnHover
+          />
+        );
+      });
     }
 
     return (
@@ -58,7 +93,7 @@ export default class MapView extends Component {
 
         {countryMarkers}
 
-        {this.state.activeCountry && (
+        {/* {this.state.activeCountry && (
           <Popup
             position={[
               this.state.activeCountry.properties.countryInfo.lat,
@@ -73,7 +108,7 @@ export default class MapView extends Component {
               <p>Deaths: {this.state.activeCountry.properties.deaths}</p>
             </div>
           </Popup>
-        )}
+        )} */}
       </Map>
     );
   }
